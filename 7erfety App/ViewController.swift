94e03009771d42655afefdc28df0bc950c5a2ce7
AHMED <ahmed@AHMEDs-MacBook-Pro.local>
2019-09-29 13:28:39 +0200

    Initial Commit

---
+++ 7erfety App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  7erfety App
+//
+//  Created by AHMED on 7/7/1398 AP.
+//  Copyright © 1398 AHMED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
